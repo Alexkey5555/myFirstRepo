1e6635072824e3250febcf0daa78a0acdcb7afc7
Alexandr Yeremieiev <sanya@hackermail.com>
2021-11-30 20:33:30 +0100

    выполнено третье задание

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title = 'JS22';
-let screens = 'Простые,Сложные,Интерактивные';
-let screenPrice = 55;
+let title = prompt('Как называеться ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 let rollback = 74;
 let fullPrice = 2500;
-let adaptive = true;
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
@@ -12,3 +12,32 @@ console.log('Стоимость верстки экранов ' + screenPrice + 
 console.log('Стоимость разработки сайта ' + fullPrice + ' долларов');
 console.log(screens.toLowerCase().split(','));
 console.log(fullPrice * (rollback / 100));
+//Lesson03
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+console.log(Math.ceil(servicePercentPrice));
+
+switch (true) {
+    case fullPrice > 30000:
+        console.log('Даем скидку в 10%');
+        break;
+    case fullPrice >= 15000 && fullPrice <= 30000:
+        console.log('Даем скидку в 5%');
+        break;
+    case fullPrice >= 0 && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break;
+    case fullPrice < 0:
+        console.log('Что то пошло не так');
+        break;
+
+};
+
+
+
+
+
