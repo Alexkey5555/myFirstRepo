a7b683587d3c123a4c4c5ee8bbd5fabfe37afcd1
Alexandr Yeremieiev <sanya@hackermail.com>
2021-12-02 20:31:36 +0100

    выполнено четвертое задание

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ let title = prompt('Как называеться ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 let screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
 let rollback = 74;
-let fullPrice = 2500;
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let allServicePrices;
 
 const showTypeOff = function (variable) {
     console.log(variable, typeof variable);
 };
-
 const getRollbackMessage = function (price) {
     switch (true) {
         case price > 30000:
@@ -31,21 +29,24 @@ const getRollbackMessage = function (price) {
             return 'Что то пошло не так';
             break;
     }
+};
+const getAllServicePrices = function (price1, price2) {
+    return allServicePrices = price1 + price2;
+};
+function getFullPrice() {
+    return fullPrice = screenPrice + allServicePrices;
+};
+function getTitle(title) {
+    title = title.trim().toLowerCase();
+    return title[0].toUpperCase() + title.slice(1);
+}
+function getServicePercentPrices() {
+    return servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 }
 
 showTypeOff(title);
 showTypeOff(screenPrice);
 showTypeOff(adaptive);
-
+console.log(screens.split(','));
 console.log(getRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(screens.toLowerCase().split(','));
-console.log(Math.ceil(servicePercentPrice));
-
-
-
-
-
+console.log(getServicePercentPrices());
