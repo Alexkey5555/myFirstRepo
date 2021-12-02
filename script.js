09ad16c9b52f79200ac92dbe85f72212519d618a
Alexandr Yeremieiev <sanya@hackermail.com>
2021-12-02 19:37:46 +0100

    чистый код

--- script.js
+++ script.js
@@ -12,23 +12,34 @@ let servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
 
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-switch (true) {
-    case fullPrice > 30000:
-        console.log('Даем скидку в 10%');
-        break;
-    case fullPrice >= 15000 && fullPrice <= 30000:
-        console.log('Даем скидку в 5%');
-        break;
-    case fullPrice >= 0 && fullPrice < 15000:
-        console.log('Скидка не предусмотрена');
-        break;
-    case fullPrice < 0:
-        console.log('Что то пошло не так');
-        break;
+const showTypeOff = function (variable) {
+    console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function (price) {
+    switch (true) {
+        case price > 30000:
+            return 'Даем скидку в 10%';
+            break;
+        case price >= 15000 && price <= 30000:
+            return 'Даем скидку в 5%';
+            break;
+        case price >= 0 && price < 15000:
+            return 'Скидка не предусмотрена';
+            break;
+        case price < 0:
+            return 'Что то пошло не так';
+            break;
+    }
 }
 
+showTypeOff(title);
+showTypeOff(screenPrice);
+showTypeOff(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
-console.log(typeof fullPrice);
+console.log(typeof screenPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 console.log(screens.toLowerCase().split(','));
